0fa895be4a3436c21b95c61b2002caad3baf0b39
nbigger <nick.bigger@gmail.com>
2018-08-18 23:25:49 -0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <title>Resume | Nick Bigger</title>
+   <link rel="stylesheet" href="css/app.css"/>
+   <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+
+   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+   <script>
+   </script>
+</head>
+
+<body>
+    <!----- HEADER START---> 
+    <header id="header">
+        <div class="content">
+            
+       </div>
+   </header>
+   <!----- SLIDES START ---> 
+   <div id="slide1">
+       <div class="content">
+          
+       </div> 
+   </div> 
+   <div id="slide2">
+       <div class="content" >
+           <div class="quotes_container">
+               <h3 class="quotes">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." </h3>
+               <img src="img/dr-seuss.png" align="left"/> <h4 class="author_name_gray">Test test </h4>
+           </div> 
+       </div> 
+   </div> 
+   <div id="slide3">
+       <div class="content">
+           <div class="quotes_container">
+               <h3 class="quotes">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam." </h3>
+               <img src="img/norman.png" align="left"/> <h4 class="author_name_white">Test test </h4>
+           </div> 
+       </div> 
+   </div> 
+   <div id="slide4">
+       <div class="content">
+           <div class="quotes_container">
+               <h3 class="quotes">"Lorem ipsum dolor sit amet, consectetur adipiscing elit..." </h3>
+               <img src="img/dr-seuss.png" align="left"/> <h4 class="author_name_gray">Test test </h4>
+           </div> 
+       </div> 
+   </div> 
+   <div id="slide5">
+       <div class="content">
+           <div class="quotes_container">
+               <h3 class="quotes">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.” </h3>
+               <img src="img/bob.png" align="left"/> <h4 class="author_name_white">Test Test </h4>
+           </div>  
+       </div> 
+   </div> 
+   <!----- SLIDES END --->
+</body>
+</html>
