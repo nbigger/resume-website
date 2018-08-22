6a68b8b0a5b69e4bcf6da405ee9af2af9e492b46
nbigger <nick.bigger@gmail.com>
2018-08-22 13:34:13 -0700

    clean up

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,32 @@
-var piesiteFired = 0;
 $(document).ready(function() {
   var $win = $(window),
   $win_height = $(window).height(),
-        // - A multiple of viewport height - The higher this number the sooner triggered.
+  
         windowPercentage = $(window).height() * 1.2;
         $win.on("scroll", scrollReveal);
         function scrollReveal() {
           var scrolled = $win.scrollTop();
 
-        ///////////////////////////////////////
-        // Bar Charts scroll activate, looking for .trigger class to fire.
-        $(".trigger").each(function() {
-          var $this = $(this),
-          offsetTop = $this.offset().top;
-          if (
-            scrolled + windowPercentage > offsetTop ||
-            $win_height > offsetTop
-            ) {
-            $(this).each(function(key, bar) {
-              var percentage = $(this).data("percentage");
-              var years = $(this).data("years");
-              $(this).css("height", percentage + "%"); 
-              $(this).text(years);
-              
-            });
+          $(".trigger").each(function() {
+            var $this = $(this),
+            offsetTop = $this.offset().top;
+            if (
+              scrolled + windowPercentage > offsetTop ||
+              $win_height > offsetTop
+              ) {
+              $(this).each(function(key, bar) {
+                var percentage = $(this).data("percentage");
+                var years = $(this).data("years");
+                $(this).css("height", percentage + "%"); 
+                $(this).text(years);
 
-        } else {
-                ///////////////////////////////////////
-                // To keep them triggered, lose this block.
-                $(this).each(function(key, bar) {
-                  $(this).css("height", 0);
-                });
-              }    
+              });
+
+          } else {
+            $(this).each(function(key, bar) {
+              $(this).css("height", 0);
             });
-      }
-    });
+          }    
+        });
+        }
+      });
